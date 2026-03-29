724d1e9ed834a470c51c509871df08fb86f09df4
Mustakim <mustakimdurvesh@gmail.com>
2026-03-30 00:38:38 +0545

    chips addded

--- api/places.js
+++ api/places.js
@@ -26,6 +26,7 @@ export default async function handler(req, res) {
             'bar',
             'wine_bar',
             'pub',
+            'restaurant',
             'bakery'
           ],
           maxResultCount: 20,
